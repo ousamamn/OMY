b5d0c85728fa6a8e3fb5a5d16a3d3b2a23b28e96
Yujieooi2000 <yujieooi2000@gmail.com>
2021-11-18 19:05:32 +0000

    Changed the binding name

--- app/src/main/java/com/example/omy/MainActivity.kt
+++ app/src/main/java/com/example/omy/MainActivity.kt
@@ -2,23 +2,24 @@ package com.example.omy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.omy.databinding.ActivityMainBinding
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.example.omy.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(R.layout.main_activity)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
