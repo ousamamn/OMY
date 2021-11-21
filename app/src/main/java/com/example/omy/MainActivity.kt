1f38999afb6e954759c1161ca069762f60d93fca
michmaml <mjsekulski1@sheffield.ac.uk>
2021-11-21 02:29:05 +0000

    Created Fragments for each activity

--- app/src/main/java/com/example/omy/MainActivity.kt
+++ app/src/main/java/com/example/omy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.omy
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
@@ -15,6 +16,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -23,15 +25,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.omy.databinding.MainActivityBinding
+import com.example.omy.fragments.HomeFragment
+import com.example.omy.fragments.LocationsFragment
+import com.example.omy.fragments.PicturesFragment
+import com.example.omy.fragments.TripsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 import okhttp3.*
 import org.json.JSONException
 import org.json.JSONObject
-import org.w3c.dom.Text
 import java.io.IOException
 import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+class MainActivity : AppCompatActivity() {//, OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var ntButton: Button
@@ -40,6 +47,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var tnEditText: EditText
     private lateinit var weatherTemperatureText: TextView
     private lateinit var weatherIconView: ImageView
+
+    private val homeFragment = HomeFragment()
+    private val locationsFragment = LocationsFragment()
+    private val tripsFragment = TripsFragment()
+    private val picturesFragment = PicturesFragment()
+
     private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,106 +61,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
-        ntButton = findViewById(R.id.new_trip_button)
-        goButton = findViewById(R.id.go_button)
-        cancelButton = findViewById(R.id.cancel_button)
-        tnEditText = findViewById(R.id.trip_name_edit_text)
-        ntButton.setOnClickListener() {
-            ntButton.visibility = View.GONE
-            goButton.visibility = View.VISIBLE
-            cancelButton.visibility = View.VISIBLE
-            tnEditText.visibility = View.VISIBLE
-        }
-        cancelButton.setOnClickListener() {
-            ntButton.visibility = View.VISIBLE
-            goButton.visibility = View.GONE
-            cancelButton.visibility = View.GONE
-            tnEditText.visibility = View.GONE
-        }
-        goButton.setOnClickListener() {
-            if (TextUtils.isEmpty(tnEditText.text.toString())) {
-                Snackbar.make(findViewById(R.id.notification_view),
-                    R.string.enter_trip_name_notification, Snackbar.LENGTH_SHORT).show()
-                it.showKeyboard()
-            } else {
-                Snackbar.make(findViewById(R.id.notification_view),
-                    R.string.successfully_created_trip, Snackbar.LENGTH_SHORT
-                ).show()
+        replaceFragment(homeFragment)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> replaceFragment(homeFragment)
+                R.id.navigation_locations -> replaceFragment(locationsFragment)
+                R.id.navigation_trips -> replaceFragment(tripsFragment)
+                R.id.navigation_pictures -> replaceFragment(picturesFragment)
             }
-        }
-
-        weatherTemperatureText = findViewById(R.id.weather_temperature)
-        weatherIconView = findViewById(R.id.weather_icon)
-        getCurrentWeather(weatherTemperatureText, weatherIconView)
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        if (currentFocus != null) {
-            val imm  = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm .hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-        }
-        return super.dispatchTouchEvent(ev)
-    }
-
-    private fun View.showKeyboard() {
-        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val sheffield = LatLng(53.38, -1.46)
-        // Tried zooming in, but no luck
-        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sheffield, 20.0f))
-        mMap.addMarker(MarkerOptions().position(sheffield).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sheffield))
-    }
-
-
-    /* --- Get weather and temperature --- */
-    private fun getCurrentWeather(textView: TextView, imageView: ImageView) {
-        // TODO: Replace lat&long with actual geolocation
-        val lat = 53.38
-        val lon = -1.46
-
-        val url = "http://api.weatherapi.com/v1/current.json?key=" + BuildConfig.WEATHER_APIKEY + "&q=" + lat + "," + lon
-        val client = OkHttpClient()
-        val request = Request.Builder().url(url).build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body!!.string()
-                runOnUiThread {
-                    try {
-                        val json = JSONObject(body)
-                        val responseObject: JSONObject = json.getJSONObject("current")
-                        val tempC = responseObject.get("temp_c")
-                        val weather = responseObject.getJSONObject("condition")
-                        val icon = weather.get("icon")
-
-                        textView.setText("${tempC.toString()}°C")
-                        loadImage(imageView, "https:$icon")
-                    } catch (e: JSONException) { e.printStackTrace() }
-                }
-            }
-            override fun onFailure(call: Call, e: IOException) { e.printStackTrace() }
-        })
-    }
-
-    private fun loadImage(icon: ImageView, url: String) {
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-        var image: Bitmap? = null
-        executor.execute {
-            try {
-                val onlineIcon = java.net.URL(url).openStream()
-                image = BitmapFactory.decodeStream(onlineIcon)
-                handler.post { icon.setImageBitmap(image) }
-            } catch (e: Exception) { e.printStackTrace() }
+            true
         }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment !== null) {
+            val transaction =  supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+    }
+
 }
--- app/src/main/java/com/example/omy/TripsActivity.kt
+++ app/src/main/java/com/example/omy/TripsActivity.kt
@@ -2,6 +2,7 @@ package com.example.omy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.omy.R
 
 class TripsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
