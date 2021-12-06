68cb2a8332499d594c2bfa7658dd6ee39565d571
Yujieooi2000 <yujieooi2000@gmail.com>
2021-12-06 21:32:01 +0000

    Added Location Fragment and the related fragments (crashed)

--- app/src/main/java/com/example/omy/fragments/LocationsFragment.kt
+++ app/src/main/java/com/example/omy/fragments/LocationsFragment.kt
@@ -5,9 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.omy.R
+import com.example.omy.locations.*
 
 class LocationsFragment : Fragment() {
+    lateinit var locationsFilterSpinner: Spinner
+    lateinit var mRecyclerView: RecyclerView
+    lateinit var mAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+    lateinit var mLayoutManager: RecyclerView.LayoutManager
+    private val locationDataset: Array<LocationElement> = arrayOf<LocationElement>(
+        LocationElement(
+            "Big monument", "-0.14", "86.67",
+            "2", "11"
+        ),
+        LocationElement(
+            "Super old tree", "85.62", "-2.90",
+            "2", "11"
+        ),
+        LocationElement(
+            "National Gallery", "33.91", "128.33",
+            "2", "11"
+        )
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +42,27 @@ class LocationsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        /*  Locations Filter Functionality */
+        locationsFilterSpinner = view.findViewById(R.id.locations_filters_spinner)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.locations_filter_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            locationsFilterSpinner.adapter = adapter
+        }
+
+        /*  Get list of locations */
+        mRecyclerView = view.findViewById<RecyclerView>(R.id.locations_list)
+        mLayoutManager = LinearLayoutManager(requireContext())
+        mRecyclerView.layoutManager = mLayoutManager
+
+        mAdapter = LocationsAdapter(locationDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+        mRecyclerView.adapter = mAdapter
+
     }
 }
---
+++ app/src/main/java/com/example/omy/locations/LocationsAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.omy.locations
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.omy.R
+
+class LocationsAdapter : RecyclerView.Adapter<LocationsAdapter.ViewHolder> {
+    private lateinit var context: Context
+    private var items: Array<LocationElement>
+
+    constructor(items: Array<LocationElement>) {
+        this.items = items
+    }
+
+    constructor(cont: Context, items: Array<LocationElement>) : super() {
+        this.items = items
+        context = cont
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //Inflate the layout, initialize the View Holder
+        val v: View = LayoutInflater.from(parent.context).inflate(
+            R.layout.location_list_item,
+            parent, false
+        )
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.titleLocation.text = items[position].titleLocation
+        holder.longitude.text = items[position].longitude
+        holder.latitude.text = items[position].latitude
+        holder.numOfReviews.text = items[position].numOfReviews
+        holder.numOfPhotos.text = items[position].numOfPhotos
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var titleLocation: TextView = itemView.findViewById<View>(R.id.location_title) as TextView
+        var longitude: TextView = itemView.findViewById<View>(R.id.longitude) as TextView
+        var latitude: TextView = itemView.findViewById<View>(R.id.latitude) as TextView
+        var numOfReviews: TextView = itemView.findViewById<View>(R.id.num_of_reviews) as TextView
+        var numOfPhotos: TextView = itemView.findViewById<View>(R.id.numOfLocations) as TextView
+
+    }
+}
