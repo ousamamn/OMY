e33742c5673a89290d1bd676fefeeb9ec5e247eb
michmaml <55076036+michmaml@users.noreply.github.com>
2021-12-06 18:57:40 +0000

    initiated switch 1/2

--- app/src/main/java/com/example/omy/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/omy/fragments/HomeFragment.kt
@@ -1,7 +1,11 @@
 package com.example.omy.fragments
 
 import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -18,13 +22,9 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.omy.BuildConfig
 import com.example.omy.R
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.snackbar.Snackbar
@@ -33,12 +33,26 @@ import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 import java.util.concurrent.Executors
-import android.content.IntentSender
+import android.location.Location
+import android.util.Log
+import androidx.fragment.app.FragmentManager
 import com.example.omy.Communicator
-import android.content.Intent as Intent
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.*
+import java.text.DateFormat
+import java.util.*
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mCurrentLocation: Location
+    private lateinit var mLastUpdateTime: String
+    private lateinit var supportFragmentManager: FragmentManager
+    private val mapView: MapView? = null
+    private var mButtonStart: Button? = null
+    private var mButtonEnd: Button? = null
+
     private lateinit var ntButton: Button
     private lateinit var goButton: Button
     private lateinit var cancelButton: Button
@@ -67,6 +81,26 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             cancelButton.visibility = View.VISIBLE
             tnEditText.visibility = View.VISIBLE
         }
+
+        mButtonStart = view.findViewById<View>(R.id.button_start) as Button
+        mButtonStart!!.setOnClickListener {
+            startLocationUpdates()
+            if (mButtonEnd != null) mButtonEnd!!.isEnabled = true
+            mButtonStart!!.isEnabled = false
+        }
+        mButtonStart!!.isEnabled = true
+        mButtonEnd = view.findViewById<View>(R.id.button_end) as Button
+        mButtonEnd!!.setOnClickListener {
+            stopLocationUpdates()
+            if (mButtonStart != null) mButtonStart!!.isEnabled = true
+            mButtonEnd!!.isEnabled = false
+        }
+        mButtonEnd!!.isEnabled = false
+
+/*        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        //startLocationUpdates()*/
 
         goButton = view.findViewById(R.id.go_button)
         goButton.setOnClickListener {
@@ -100,6 +134,108 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         getCurrentWeather(weatherTemperatureText, weatherIconView)
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14.0f))
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireContext() as Activity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    requireContext() as Activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    ACCESS_FINE_LOCATION
+                )
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return
+        }
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallback,
+            null  /*Looper*/
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mLocationRequest = LocationRequest.create().apply {
+            interval = 1000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        startLocationUpdates()
+    }
+
+
+    private var mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            mCurrentLocation = locationResult.getLastLocation()
+            mLastUpdateTime = DateFormat.getTimeInstance().format(Date())
+            Log.i("MAP", "new location " + mCurrentLocation.toString())
+            if (mMap != null) mMap.addMarker(
+                MarkerOptions().position(
+                    LatLng(
+                        mCurrentLocation!!.latitude,
+                        mCurrentLocation!!.longitude
+                    )
+                ).title(mLastUpdateTime)
+            )
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(
+                        mCurrentLocation!!.latitude,
+                        mCurrentLocation!!.longitude
+                    ), 14.0f
+                )
+            )
+        }
+    }
+
+    /**
+     * it stops the location updates
+     */
+    private fun stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback)
+    }
+
     private fun closeKeyboard(view: View) {
         val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken,0)
@@ -113,42 +249,15 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     }
 
     /* --- Set up the map --- */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val sheffield = LatLng(53.38, -1.46)
-        enableMyLocation()
 
-        // Tried zooming in, but no luck
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sheffield, 15f))
-        mMap.addMarker(MarkerOptions().position(sheffield).title("Marker in Sydney"))
-    }
 
-    private fun isPermissionGranted() : Boolean {
-        return ContextCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun enableMyLocation() {
-        if (isPermissionGranted()) {
-            mMap.isMyLocationEnabled = true
-        }
-        else {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION_PERMISSION
-            )
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    /*override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == REQUEST_LOCATION_PERMISSION) {
             if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
                 enableMyLocation()
             }
         }
-    }
+    }*/
 
     /* --- Get weather and temperature --- */
     private fun getCurrentWeather(textView: TextView, imageView: ImageView) {
