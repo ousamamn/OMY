2b862bd6af59b99810d0abb061f884913b61f3b7
Yujieooi2000 <yujieooi2000@gmail.com>
2021-12-07 17:10:10 +0000

    Update the location show activity and its layout

--- app/src/main/java/com/example/omy/locations/LocationShowActivity.kt
+++ app/src/main/java/com/example/omy/locations/LocationShowActivity.kt
@@ -1,12 +1,17 @@
 package com.example.omy.locations
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import com.example.omy.R
 import com.example.omy.locations.LocationsAdapter.Companion.items
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class LocationShowActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.location_show_activity)
@@ -17,13 +22,39 @@ class LocationShowActivity : AppCompatActivity() {
             position = b.getInt("position")
             if (position != -1) {
                 val textView = findViewById<TextView>(R.id.title_name)
+                val textViewTitleInfo = findViewById<TextView>(R.id.name_title_info)
+                val textViewLongitudeInfo = findViewById<TextView>(R.id.longitude_info)
+                val textViewLatitudeInfo = findViewById<TextView>(R.id.latitude_info)
                 val element = LocationsAdapter.items[position]
                 if (element != null) {
                     textView.text = element.titleLocation
+                    textViewTitleInfo.text = element.titleLocation
+                    textViewLongitudeInfo.text = element.longitude
+                    textViewLatitudeInfo.text = element.latitude
                 } else if (element == null) {
                     textView.text = "Error"
+                    textViewTitleInfo.text = "Error"
+                    textViewLongitudeInfo.text = "Error"
+                    textViewLatitudeInfo.text = "Error"
                 }
             }
+        }
+
+        val closeOptionsButton = findViewById<FloatingActionButton>(R.id.options_close_button)
+        val openOptionsButton = findViewById<FloatingActionButton>(R.id.options_open_button)
+        val editReviewButton = findViewById<FloatingActionButton>(R.id.edit_review_button)
+        val addPhotoButton = findViewById<FloatingActionButton>(R.id.add_photo_button)
+        openOptionsButton.setOnClickListener {
+            closeOptionsButton.visibility = View.VISIBLE
+            editReviewButton.visibility = View.VISIBLE
+            addPhotoButton.visibility = View.VISIBLE
+            openOptionsButton.visibility = View.GONE
+        }
+        closeOptionsButton.setOnClickListener {
+            closeOptionsButton.visibility = View.GONE
+            editReviewButton.visibility = View.GONE
+            addPhotoButton.visibility = View.GONE
+            openOptionsButton.visibility = View.VISIBLE
         }
     }
 }
