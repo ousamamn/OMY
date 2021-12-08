fdcb4a979bafeb82b113cee05814e9196cb75e73
michmaml <55076036+michmaml@users.noreply.github.com>
2021-12-08 21:15:44 +0000

    Redesigned the scrollpane for trip

--- app/src/main/java/com/example/omy/trips/TripShowActivity.kt
+++ app/src/main/java/com/example/omy/trips/TripShowActivity.kt
@@ -15,22 +15,15 @@ class TripShowActivity : AppCompatActivity() {
         if (b != null) {
             position = b.getInt("position")
             if (position != -1) {
-                val textView = findViewById<TextView>(R.id.title_name)
-                val textViewDate = findViewById<TextView>(R.id.date_info)
-                val textViewDistance = findViewById<TextView>(R.id.distance_info)
-                val textViewLocation = findViewById<TextView>(R.id.numOfLocations_info)
+                val tripTitle = findViewById<TextView>(R.id.title_name)
+                val tripDate = findViewById<TextView>(R.id.date_info)
+                val tripDistance = findViewById<TextView>(R.id.distance_info)
+                val tripLocation = findViewById<TextView>(R.id.numOfLocations_info)
                 val element = TripsAdapter.items[position]
-                if (element != null) {
-                    textView.text = element.title
-                    textViewDate.text = element.date
-                    textViewDistance.text = element.distance + " km"
-                    textViewLocation.text = element.numOfLocations
-                } else if (element == null) {
-                    textView.text = "Error"
-                    textViewDate.text = "Error"
-                    textViewDistance.text = "Error"
-                    textViewLocation.text = "Error"
-                }
+                tripTitle.text = element.title
+                tripDate.text = element.date
+                tripDistance.text = element.distance + " km"
+                tripLocation.text = element.numOfLocations
             }
         }
     }
