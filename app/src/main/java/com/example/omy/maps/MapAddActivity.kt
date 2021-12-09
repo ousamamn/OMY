dfe78b0916d8ef68c6832fef6dd2c9a9b7b0227d
Yujieooi2000 <yujieooi2000@gmail.com>
2021-12-09 23:33:09 +0000

    Enable photoshoting in map add activity

--- app/src/main/java/com/example/omy/maps/MapAddActivity.kt
+++ app/src/main/java/com/example/omy/maps/MapAddActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.omy.R
@@ -12,9 +14,11 @@ class MapAddActivity : AppCompatActivity() {
     private lateinit var displayTitle: TextView
     private lateinit var cancelButton: Button
     private lateinit var saveButton: Button
+    private lateinit var addFun: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map_add_activity)
+        val intentPhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         val b: Bundle? = intent.extras
 
         var msg: String? = "Title"
@@ -25,11 +29,15 @@ class MapAddActivity : AppCompatActivity() {
         }
         cancelButton = findViewById(R.id.cancel_button)
         saveButton = findViewById(R.id.save_button)
+        addFun = findViewById(R.id.add_photo_view)
         cancelButton.setOnClickListener {
             onBackPressed()
         }
         saveButton.setOnClickListener {
             onBackPressed()
+        }
+        addFun.setOnClickListener {
+            startActivityForResult(intentPhoto, 1)
         }
     }
 }
