d81e8833358e961efb5677b80631b3c23dfdd3d7
Yujieooi2000 <yujieooi2000@gmail.com>
2021-12-09 21:58:44 +0000

    Update the cancel button and save button functionality but the save button is not linked to the database

--- app/src/main/java/com/example/omy/maps/MapAddActivity.kt
+++ app/src/main/java/com/example/omy/maps/MapAddActivity.kt
@@ -1,13 +1,17 @@
 package com.example.omy.maps
 
+import android.content.Intent
 import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.omy.R
 
 class MapAddActivity : AppCompatActivity() {
     private lateinit var displayTitle: TextView
+    private lateinit var cancelButton: Button
+    private lateinit var saveButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map_add_activity)
@@ -18,6 +22,14 @@ class MapAddActivity : AppCompatActivity() {
             msg = b.getString("msg")
             displayTitle = findViewById(R.id.display_title_map_add)
             displayTitle.text = msg
+        }
+        cancelButton = findViewById(R.id.cancel_button)
+        saveButton = findViewById(R.id.save_button)
+        cancelButton.setOnClickListener {
+            onBackPressed()
+        }
+        saveButton.setOnClickListener {
+            onBackPressed()
         }
     }
 }
