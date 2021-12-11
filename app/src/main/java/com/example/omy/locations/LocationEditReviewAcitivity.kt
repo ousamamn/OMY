0243d08701fd5fa8eee2dd0b5b4e2d8134f42b0e
Yujieooi2000 <yujieooi2000@gmail.com>
2021-12-11 17:35:46 +0000

    Update the hide soft keyboard function when not using the edit text for LocationEditReviewAcitivity.kt and MapAddActivity.kt Update the input type for map_add_activity.xml

--- app/src/main/java/com/example/omy/locations/LocationEditReviewAcitivity.kt
+++ app/src/main/java/com/example/omy/locations/LocationEditReviewAcitivity.kt
@@ -1,16 +1,21 @@
 package com.example.omy.locations
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.Spanned
 import android.text.TextUtils
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.omy.R
+import com.example.omy.maps.MapAddActivity
 import com.example.omy.maps.MapsCreatedActivity
 import com.google.android.material.snackbar.Snackbar
 import java.lang.NumberFormatException
@@ -63,6 +68,7 @@ class LocationEditReviewAcitivity : AppCompatActivity() {
                 //context?.startActivity(intent)
             }
         }
+        setupUI(findViewById(R.id.on_touch_review))
     }
 
     inner class MinMaxFilter() : InputFilter {
@@ -95,6 +101,38 @@ class LocationEditReviewAcitivity : AppCompatActivity() {
 
         private fun isInRange(a: Int, b: Int, c: Int): Boolean {
             return if (b > a) c in a..b else c in b..a
+        }
+    }
+
+    private fun hideSoftKeyboard(locationEditReviewAcitivity: LocationEditReviewAcitivity) {
+        val inputMethodManager: InputMethodManager = getSystemService(
+            INPUT_METHOD_SERVICE
+        ) as InputMethodManager
+        if (inputMethodManager.isAcceptingText) {
+            inputMethodManager.hideSoftInputFromWindow(
+                currentFocus!!.windowToken,
+                0
+            )
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupUI(view: View) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (view !is EditText) {
+            view.setOnTouchListener(View.OnTouchListener { v, event ->
+                hideSoftKeyboard(this)
+                false
+            })
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view is ViewGroup) {
+            for (i in 0 until (view as ViewGroup).childCount) {
+                val innerView: View = (view as ViewGroup).getChildAt(i)
+                setupUI(innerView)
+            }
         }
     }
 }
--- app/src/main/java/com/example/omy/maps/MapAddActivity.kt
+++ app/src/main/java/com/example/omy/maps/MapAddActivity.kt
@@ -1,12 +1,23 @@
 package com.example.omy.maps
 
-import android.content.Intent
-import android.icu.text.CaseMap
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.text.TextUtils
 import com.example.omy.R
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import android.view.ViewGroup
+
+import android.view.View
+
+import android.view.View.OnTouchListener
+
+import android.widget.EditText
+
+
+
+
 
 class MapAddActivity : AppCompatActivity() {
     private lateinit var displayTitle: TextView
@@ -23,6 +34,8 @@ class MapAddActivity : AppCompatActivity() {
             displayTitle = findViewById(R.id.display_title_map_add)
             displayTitle.text = msg
         }
+        val titleNameEditText = findViewById<EditText>(R.id.title_name_edit_text)
+        val descriptionEditText = findViewById<EditText>(R.id.description_name_edit_text)
         cancelButton = findViewById(R.id.cancel_button)
         saveButton = findViewById(R.id.save_button)
         cancelButton.setOnClickListener {
@@ -30,8 +43,61 @@ class MapAddActivity : AppCompatActivity() {
             finish()
         }
         saveButton.setOnClickListener {
-            onBackPressed()
-            finish()
+            if (TextUtils.isEmpty(titleNameEditText.text.toString())) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please Insert The Title",
+                    Toast.LENGTH_SHORT
+                )
+                    .show();
+            } else if (TextUtils.isEmpty(descriptionEditText.text.toString())) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please Insert The Description",
+                    Toast.LENGTH_SHORT
+                )
+                    .show();
+            } else {
+                onBackPressed()
+                finish()
+                // the onBackPressed need to be change to be able to save the reviews
+                //val intent = Intent(context, MapsCreatedActivity::class.java)
+                //val msg = tnEditText.text.toString()
+                //intent.putExtra("msg", msg)
+                //context?.startActivity(intent)
+            }
+        }
+        setupUI(findViewById(R.id.on_touch))
+    }
+    private fun hideSoftKeyboard(mapAddActivity: MapAddActivity) {
+        val inputMethodManager: InputMethodManager = getSystemService(
+            INPUT_METHOD_SERVICE
+        ) as InputMethodManager
+        if (inputMethodManager.isAcceptingText) {
+            inputMethodManager.hideSoftInputFromWindow(
+                currentFocus!!.windowToken,
+                0
+            )
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupUI(view: View) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (view !is EditText) {
+            view.setOnTouchListener(OnTouchListener { v, event ->
+                hideSoftKeyboard(this)
+                false
+            })
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view is ViewGroup) {
+            for (i in 0 until (view as ViewGroup).childCount) {
+                val innerView: View = (view as ViewGroup).getChildAt(i)
+                setupUI(innerView)
+            }
         }
     }
 }
