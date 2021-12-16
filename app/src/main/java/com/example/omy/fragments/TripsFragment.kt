69e2e86ebc0d905d247b4594beb3c6a51bca5d95
michmaml <55076036+michmaml@users.noreply.github.com>
2021-12-16 13:06:15 +0000

    Updated the file with 2 missing imports

--- app/src/main/java/com/example/omy/fragments/TripsFragment.kt
+++ app/src/main/java/com/example/omy/fragments/TripsFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.omy.R
