fbe91fea32dfae4e3e0e9f0af83bf3eea4144c32
michmaml <55076036+michmaml@users.noreply.github.com>
2021-12-20 00:17:14 +0000

    Added locations filtering (A-Z by default)

--- app/src/main/java/com/example/omy/fragments/LocationsFragment.kt
+++ app/src/main/java/com/example/omy/fragments/LocationsFragment.kt
@@ -1,55 +1,34 @@
 package com.example.omy.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.omy.R
-import com.example.omy.data.Image
 import com.example.omy.data.Location
 import com.example.omy.locations.*
-import com.example.omy.photos.PhotosAdapter
-import com.example.omy.photos.PhotosViewModel
 import com.example.omy.trips.TripsAdapter
-import com.example.omy.trips.TripsViewModel
 import java.util.ArrayList
 
-class LocationsFragment : Fragment() {
-    lateinit var locationsFilterSpinner: Spinner
-    lateinit var mRecyclerView: RecyclerView
-    private lateinit var mAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
-    lateinit var mLayoutManager: RecyclerView.LayoutManager
-    private var locationsViewModel: LocationsViewModel? = null
-    private val locationsDataset: MutableList<Location?> = ArrayList<Location?>()
-    /*private val locationDataset: Array<LocationElement> = arrayOf<LocationElement>(
-        LocationElement(
-            "Big monument", "-0.14", "86.67",
-            "2", "11"
-        ),
-        LocationElement(
-            "Super old tree", "85.62", "-2.90",
-            "2", "11"
-        ),
-        LocationElement(
-            "National Gallery", "33.91", "128.33",
-            "2", "11"
-        ),
-        LocationElement(
-            "National Gallery", "33.91", "128.33",
-            "2", "11"
-        )
-    )*/
+class LocationsFragment : Fragment(), AdapterView.OnItemSelectedListener {
+    lateinit var locationsSortSpinner: Spinner
+    private var locationsDataset: List<Location?> = ArrayList<Location?>()
+    private var sortedLocationsDataset: List<Location?> = ArrayList<Location?>()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private var locationsViewModel: LocationsViewModel? = null
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_locations, container, false)
     }
@@ -57,18 +36,16 @@ class LocationsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*  Locations Filter Functionality */
-        locationsFilterSpinner = view.findViewById(R.id.locations_filters_spinner)
+        /*  Locations Sort Functionality */
+        val locationsSortSpinner: Spinner = view.findViewById(R.id.locations_filters_spinner)
+        locationsSortSpinner.onItemSelectedListener = this
+
         locationsViewModel = ViewModelProvider(this)[LocationsViewModel::class.java]
-        ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.locations_filter_array,
+        ArrayAdapter.createFromResource(requireContext(), R.array.locations_filter_array,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            locationsFilterSpinner.adapter = adapter
+            locationsSortSpinner.adapter = adapter
         }
 
         /*  Get list of locations */
@@ -77,18 +54,57 @@ class LocationsFragment : Fragment() {
         mRecyclerView.layoutManager = mLayoutManager
         mAdapter = LocationsAdapter(locationsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
         initData()
-
-        //mAdapter = LocationsAdapter(locationDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
         mRecyclerView.adapter = mAdapter
+
+        if (locationsDataset.isNotEmpty()) {
+            Log.i("TAG", locationsDataset[0]!!.locationTitle!!)
+            LocationsAdapter(locationsDataset)
+            Log.i("another", TripsAdapter.items[0]!!.tripTitle!!)
+        }
     }
 
     private fun initData() {
         this.locationsViewModel!!.getLocationsToDisplay()!!.observe(viewLifecycleOwner, { newValue ->
-            //tripsDataset = newValue
+            locationsDataset = newValue
             mAdapter.notifyDataSetChanged()
-            mAdapter = LocationsAdapter(newValue) as RecyclerView.Adapter<RecyclerView.ViewHolder>
-
+            //mAdapter = LocationsAdapter(locationsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
             //mRecyclerView.adapter = mAdapter
         })
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (position) {
+            0 -> {
+                Log.e("p0","clicked a-z")
+                sortedLocationsDataset =
+                    locationsDataset.sortedBy { it!!.locationTitle } as MutableList<Location>
+            }
+            1 -> {
+                Log.e("p0","clicked z-a")
+                sortedLocationsDataset =
+                    locationsDataset.sortedByDescending { it!!.locationTitle } as MutableList<Location>
+            }
+            2 -> {
+                Log.e("p0","clicked newest")
+                sortedLocationsDataset =
+                    locationsDataset.sortedBy { it!!.locationLatitude } as MutableList<Location>
+            }
+            3 -> {
+                Log.e("p0","clicked oldest")
+                sortedLocationsDataset =
+                    locationsDataset.sortedByDescending { it!!.locationLatitude } as MutableList<Location>
+            }
+            else -> {
+                //set the list of locations to 0, A-Z
+                sortedLocationsDataset = locationsDataset
+            }
+        }
+        mAdapter = LocationsAdapter(sortedLocationsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+        mRecyclerView.adapter = mAdapter
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        mAdapter = LocationsAdapter(locationsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+        mRecyclerView.adapter = mAdapter
     }
 }
