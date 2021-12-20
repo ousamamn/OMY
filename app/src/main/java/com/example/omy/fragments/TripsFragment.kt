456fdd0b320b9b35490a36e79e9cacbb6cff2ab8
michmaml <55076036+michmaml@users.noreply.github.com>
2021-12-20 00:32:35 +0000

    Added trips sorting (titles and dates)

--- app/src/main/java/com/example/omy/fragments/TripsFragment.kt
+++ app/src/main/java/com/example/omy/fragments/TripsFragment.kt
@@ -6,32 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.omy.R
 import com.example.omy.trips.*
 import com.example.omy.data.Trip
-import com.example.omy.data.TripDao
-import kotlinx.coroutines.*
-import com.example.omy.MainRepository
 
-
-class TripsFragment : Fragment() {
-    lateinit var tripsFilterSpinner: Spinner
+class TripsFragment : Fragment(), AdapterView.OnItemSelectedListener {
+    private lateinit var tripsSortSpinner: Spinner
     private var tripsDataset: List<Trip?> = ArrayList<Trip?>()
+    private var sortedTripsDataset: List<Trip?> = ArrayList<Trip?>()
+
+
     private var tripsViewModel: TripsViewModel? = null
     lateinit var mRecyclerView: RecyclerView
     lateinit var mAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
     lateinit var mLayoutManager: RecyclerView.LayoutManager
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trips, container, false)
     }
@@ -39,21 +35,20 @@ class TripsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val trip1 = Trip(id = "test", tripTitle = "Me at the Zoo", tripDate = "12 Dec 2021",
-            tripDistance = 3.2, tripWeather = "19.0", tripDescription = "description", tripListCoords = "test")
-        tripsViewModel = ViewModelProvider(this)[TripsViewModel::class.java]
-        //tripsDataset.clear()
-
-        //val location = Location(id=55,locationTitle = "title",locationDescription = "description",locationLatitude = 1.2,locationLongitude = 1.1,locationTripId = 34)
-
-        /*  Trips Filter Functionality */
-        tripsFilterSpinner = view.findViewById(R.id.trips_filters_spinner)
-        ArrayAdapter.createFromResource(requireContext(), R.array.trips_filter_array,
+        /*  Locations Sort Functionality */
+        tripsSortSpinner = view.findViewById(R.id.trips_filters_spinner)
+        tripsSortSpinner.onItemSelectedListener = this
+        ArrayAdapter.createFromResource(requireContext(), R.array.sort_array,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            tripsFilterSpinner.adapter = adapter
+            tripsSortSpinner.adapter = adapter
         }
+
+        //val trip1 = Trip(id = "test", tripTitle = "Me at the Zoo", tripDate = "12 Dec 2021", tripDistance = 3.2, tripWeather = "19.0", tripDescription = "description", tripListCoords = "test")
+        tripsViewModel = ViewModelProvider(this)[TripsViewModel::class.java]
+        //tripsDataset.clear()
+        //val location = Location(id=55,locationTitle = "title",locationDescription = "description",locationLatitude = 1.2,locationLongitude = 1.1,locationTripId = 34)
 
         /*  Get list of trips */
         mRecyclerView = view.findViewById<RecyclerView>(R.id.trips_list)
@@ -61,19 +56,8 @@ class TripsFragment : Fragment() {
         mRecyclerView.layoutManager = mLayoutManager
         mAdapter = TripsAdapter(tripsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
         initData()
-        /*tripsViewModel!!.getTripsToDisplay()?.observe(viewLifecycleOwner, { newValue ->
-            myDataset = newValue
-            mAdapter = TripsAdapter(myDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
-            mRecyclerView.adapter = mAdapter
-            //newDataSet.addAll(myDataset)
-
-        }
-        )
-        if (myDataset.isNotEmpty()){
-            newDataSet.add((myDataset[1]))
-        }*/
-
         mRecyclerView.adapter = mAdapter
+
         if (tripsDataset.isNotEmpty()) {
             Log.i("TAG", tripsDataset[0]!!.tripTitle!!)
             TripsAdapter(tripsDataset)
@@ -85,7 +69,43 @@ class TripsFragment : Fragment() {
         this.tripsViewModel!!.getTripsToDisplay()!!.observe(viewLifecycleOwner, { newValue ->
             tripsDataset = newValue
             mAdapter.notifyDataSetChanged()
-            mAdapter = TripsAdapter(newValue) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+            //mAdapter = TripsAdapter(newValue) as RecyclerView.Adapter<RecyclerView.ViewHolder>
         })
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (position) {
+            0 -> {
+                Log.e("p0","clicked a-z")
+                sortedTripsDataset =
+                    tripsDataset.sortedBy { it!!.tripTitle } as MutableList<Trip>
+            }
+            1 -> {
+                Log.e("p0","clicked z-a")
+                sortedTripsDataset =
+                    tripsDataset.sortedByDescending { it!!.tripTitle } as MutableList<Trip>
+            }
+            2 -> {
+                Log.e("p0","clicked newest")
+                sortedTripsDataset =
+                    tripsDataset.sortedBy { it!!.tripDate } as MutableList<Trip>
+            }
+            3 -> {
+                Log.e("p0","clicked oldest")
+                sortedTripsDataset =
+                    tripsDataset.sortedByDescending { it!!.tripDate } as MutableList<Trip>
+            }
+            else -> {
+                //set the list of locations to 0, A-Z
+                sortedTripsDataset = tripsDataset
+            }
+        }
+        mAdapter = TripsAdapter(sortedTripsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+        mRecyclerView.adapter = mAdapter
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        mAdapter = TripsAdapter(tripsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+        mRecyclerView.adapter = mAdapter
     }
 }
