87aa2ff2cf0de1159314a72342c5422988f6bd7a
michmaml <55076036+michmaml@users.noreply.github.com>
2021-12-21 15:05:08 +0000

    Display all reviews (will change it to onl 2 most recent two) in the list of all reviews activity

--- app/src/main/java/com/example/omy/reviews/LocationReviewsActivity.kt
+++ app/src/main/java/com/example/omy/reviews/LocationReviewsActivity.kt
@@ -2,16 +2,40 @@ package com.example.omy.reviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.omy.R
+import com.example.omy.data.Review
+import java.util.ArrayList
 
 class LocationReviewsActivity : AppCompatActivity() {
+    private lateinit var mRecyclerViewReviews: RecyclerView
+    private lateinit var mReviewsAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+    private lateinit var mReviewsLayoutManager: RecyclerView.LayoutManager
+    private var reviewsDataset: List<Review?> = ArrayList<Review?>()
+    private var reviewsViewModel: ReviewsViewModel? = null
+    private lateinit var reviewsRecyclerEmpty: TextView
+
     private lateinit var displayTitle: TextView
     private lateinit var backToPrevious: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.location_reviews_activity)
+
+        reviewsRecyclerEmpty = findViewById(R.id.no_reviews)
+        mRecyclerViewReviews = findViewById(R.id.location_reviews)
+        mReviewsLayoutManager = LinearLayoutManager(this)
+        mRecyclerViewReviews.layoutManager = mReviewsLayoutManager
+        mReviewsAdapter = LocationReviewsAdapter(reviewsDataset) as RecyclerView.Adapter<RecyclerView.ViewHolder>
+        initDataReviews()
+        if (reviewsDataset.isNotEmpty()) {
+            LocationReviewsAdapter(reviewsDataset)
+        }
+
+
         val b: Bundle? = intent.extras
 
         var locationTitle: String? = "Title"
@@ -25,7 +49,14 @@ class LocationReviewsActivity : AppCompatActivity() {
             onBackPressed()
             finish()
         }
+    }
 
-
+    private fun initDataReviews() {
+        this.reviewsViewModel!!.getReviewsToDisplay()!!.observe(viewLifecycleOwner, { newValue ->
+            reviewsDataset = newValue
+            mReviewsAdapter.notifyDataSetChanged()
+            if (newValue.isEmpty()) reviewsRecyclerEmpty.visibility = View.VISIBLE
+            else reviewsRecyclerEmpty.visibility = View.GONE
+        })
     }
 }
